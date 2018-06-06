50b2fed3d7df00429cc65c3269fab8bf29ab4f9d
John <kiyoto_zy@hotmail.com>
2018-06-06 18:46:52 +0800

    add cancel/back action

--- app/src/main/java/and/travelingkp/com/andvertravelingkpfactory/HomeSearchPage.java
+++ app/src/main/java/and/travelingkp/com/andvertravelingkpfactory/HomeSearchPage.java
@@ -3,13 +3,26 @@ package and.travelingkp.com.andvertravelingkpfactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class HomeSearchPage extends AppCompatActivity{
+
+    TextView cancelBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_searchpage);
+
+        cancelBtn = findViewById(R.id.cancelTxt);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+            }
+        });
     }
 
 }
